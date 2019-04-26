4170096b0cea21c889eb3702f84fe4919daf30ba
Vadym Oliinyk <vadym.oliinyk@zoho.eu>
2019-04-26 15:25:17 +0300

    Initial markets class implementation

--- demo/main.cpp
+++ demo/main.cpp
@@ -1,6 +1,12 @@
 #include <fcpp/fcpp.h>
+#include <iostream>
+
+#include "fcpp/markets.h"
 
 int main()
 {
-    return 0;
+    Markets markets;
+    std::cout << markets.get_all() << std::endl;
+
+return 0;
 }
--- src/markets.cpp
+++ src/markets.cpp
@@ -1,7 +1,8 @@
 #include "fcpp/markets.h"
 #include "cpr/cpr.h"
-
+#include <iostream>
 
 std::string Markets::get_all() {
-
+        auto r = cpr::Get(cpr::Url{"https://jsonplaceholder.typicode.com/posts"});
+        return r.text;
 }
